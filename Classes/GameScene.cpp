2b5da6a4f28479b5466a9d25d110ce064d4e92c3
Lion3 <315966692@qq.com>
2017-07-09 21:59:59 +0800

    添加动画

--- Classes/GameScene.cpp
+++ Classes/GameScene.cpp
@@ -31,8 +31,9 @@ bool GameScene::init()
     
     auto visibleSize = Director::getInstance()->getVisibleSize();
     Vec2 origin = Director::getInstance()->getVisibleOrigin();
-
+	
 	loadSprites();
+	loadAnimation();
 	initGrid();
     // position the sprite on the center of the screen
 	float backGroundImageScaleX = visibleSize.width / m_backGroundImage->getContentSize().width;
@@ -40,15 +41,12 @@ bool GameScene::init()
 	m_backGroundImage->setScaleX(backGroundImageScaleX);
 	m_backGroundImage->setScaleY(backGroundImageScaleY);
 	m_backGroundImage->setPosition(Vec2(visibleSize.width / 2 + origin.x, visibleSize.height / 2 + origin.y));
-/*	float gridWidth = m_solvedGridImage->getContentSize().width;
-	float gridHeight = m_solvedGridImage->getContentSize().height;
-	m_solvedGridImage->setPosition(Vec2(visibleSize.width / 2 + origin.x - gridWidth, visibleSize.height / 2 + origin.y));
-	m_normalGridImage->setPosition(Vec2(visibleSize.width / 2 + origin.x + gridWidth, visibleSize.height / 2 + origin.y));
-   */ 
+
+	// 
+	m_hamsterGrid[3][3]->runAction(Sequence::create(m_hamsterAppearAnimate,m_hamsterDisappearAnimate,NULL));
    // add the sprite as a child to this layer
     this->addChild(m_backGroundImage, 0);
-	//this->addChild(m_solvedGridImage, 1);
-	//this->addChild(m_normalGridImage, 1);
+
     return true;
 }
 
@@ -61,6 +59,28 @@ void GameScene::preloadMusic()
 	sae->preloadBackgroundMusic(m_backGroundMusicFile);
 	
 	sae->playBackgroundMusic(m_backGroundMusicFile, true);
+}
+
+void GameScene::loadAnimation()
+{
+	Animation* appear_animation = Animation::create();
+	appear_animation->addSpriteFrameWithFileName(".././Resources/marmot_1.png");
+	appear_animation->addSpriteFrameWithFileName(".././Resources/marmot_2.png");
+	appear_animation->addSpriteFrameWithFileName(".././Resources/marmot_3.png");
+	appear_animation->setDelayPerUnit(0.1f);//设置动画的间隔时间  
+	appear_animation->setRestoreOriginalFrame(false);//是否返回第一帧  
+	m_hamsterAppearAnimate = Animate::create(appear_animation);
+
+	Animation* disappear_animation = Animation::create();
+	disappear_animation->addSpriteFrameWithFileName(".././Resources/marmot_4.png");
+	disappear_animation->addSpriteFrameWithFileName(".././Resources/marmot_3.png");
+	disappear_animation->addSpriteFrameWithFileName(".././Resources/marmot_2.png");
+	disappear_animation->addSpriteFrameWithFileName(".././Resources/marmot_1.png");
+	disappear_animation->addSpriteFrameWithFileName(".././Resources/marmot_5.png");
+	disappear_animation->setDelayPerUnit(0.1f);//设置动画的间隔时间  
+	disappear_animation->setRestoreOriginalFrame(false);//是否返回第一帧  
+	m_hamsterDisappearAnimate = Animate::create(disappear_animation);
+
 }
 
 void GameScene::loadSprites()
@@ -84,6 +104,8 @@ void GameScene::initGrid()
 				Vec2(visibleSize.width / 3 + origin.x + i * gridWidth + 2 * gridWidth,
 					visibleSize.height * 2 / 3 + origin.y - j * gridHeight - gridHeight));
 			this->addChild(m_solvedGrid[i][j], 1);
+			
+			// 是否生成地雷格子
 
 			//m_mineGrid[i][j] = Sprite::create(".././Resources/mine.PNG");
 			//m_mineGrid[i][j]->setPosition(
@@ -97,20 +119,33 @@ void GameScene::initGrid()
 			m_mineNumberGrid[i][j] = Label::createWithTTF(ttfConfig, number_str);
 			m_mineNumberGrid[i][j]->setPosition(Vec2(visibleSize.width / 3 + origin.x + i * gridWidth + 2 * gridWidth,
 				visibleSize.height * 2 / 3 + origin.y - j * gridHeight - gridHeight));
-			//m_mineNumberGrid[i][j]->setString("Reset String");
+
+			// 是否显示周围地雷数的格子
+			m_mineNumberGrid[i][j]->setVisible(false);
+
 			this->addChild(m_mineNumberGrid[i][j], 3);
 
-	/*		m_normalGrid[i][j] = Sprite::create(".././Resources/grid.PNG");
+			m_normalGrid[i][j] = Sprite::create(".././Resources/grid.PNG");
 			m_normalGrid[i][j]->setPosition(
 				Vec2(visibleSize.width / 3 + origin.x + i * gridWidth + 2 * gridWidth,
 					visibleSize.height * 2 / 3 + origin.y - j * gridHeight -  gridHeight));
-			this->addChild(m_normalGrid[i][j], 4);*/
+			this->addChild(m_normalGrid[i][j], 4);
 
-			
+			m_hamsterGrid[i][j] = Sprite::create();
+			m_hamsterGrid[i][j]->setPosition(
+				Vec2(visibleSize.width / 3 + origin.x + i * gridWidth + 2 * gridWidth,
+					visibleSize.height * 2 / 3 + origin.y - j * gridHeight - gridHeight));
+			this->addChild(m_hamsterGrid[i][j], 5);
 			
 		}
 	}
 	
+}
+
+void GameScene::setMineNumberGrid(int x, int y, int value)
+{
+	std::string number_str = StringUtils::format("%d", value);
+	m_mineNumberGrid[x][y]->setString(number_str);
 }
 
 
--- Classes/GameScene.h
+++ Classes/GameScene.h
@@ -12,19 +12,20 @@ class GameScene : public cocos2d::Layer
 public:
     static cocos2d::Scene* createScene();
 
-    virtual bool init();
-
-	// 以下由UI部分完成
-	void preloadMusic();					// 预加载音乐
-	void loadAnimation(string filepath);	// 加载动画
-	void loadSprites();						// 加载精灵
-	void initGrid();
+    virtual bool init();	
 
     // implement the "static create()" method manually
     CREATE_FUNC(GameScene);
 
 private:
+
+	// 以下由UI部分完成
+	void preloadMusic();					// 预加载音乐
+	void loadAnimation();	// 加载动画
+	void loadSprites();						// 加载精灵
+	void initGrid();
 	const static int gridSize = 9;
+	void setMineNumberGrid(int x,int y, int value);
 	Sprite* m_backGroundImage;
 	Sprite* m_hamsterImage;
 	Sprite* m_mineImage;
@@ -41,8 +42,11 @@ private:
 	Label* m_time;
 	Label* m_score;
 	
-	SpriteFrame* m_hamsterAppearFrame;
-	SpriteFrame* m_hamsterDisappearFrame;
+	Vector<SpriteFrame*> m_hamsterAppearFrame;
+	Animate* m_hamsterAppearAnimate;
+	Vector<SpriteFrame*> m_hamsterDisappearFrame;
+	Animate* m_hamsterDisappearAnimate;
+
 	SpriteFrame* m_hitFrame;
 
 	char* m_hitMineEffectFile = "";
